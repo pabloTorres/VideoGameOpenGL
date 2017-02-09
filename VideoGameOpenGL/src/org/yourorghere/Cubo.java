bb37e8e89edb47ff2831b00e3b58ac00b87efa2a
Pablo <Pablo@Adminstrador>
2017-02-09 16:09:27 +0100

    Posibilidad de texturizar cubos (laberinto). Interfaz Movil que facilitará la familiarización de objetos para colisiones

--- VideoGameOpenGL/src/org/yourorghere/Cubo.java
+++ VideoGameOpenGL/src/org/yourorghere/Cubo.java
@@ -5,23 +5,35 @@
  */
 package org.yourorghere;
 
+import com.sun.opengl.util.texture.Texture;
 import javax.media.opengl.GL;
 
 /**
  *
  * @author fing.labcom
  */
-public class Cubo {
+public class Cubo implements Movil {
 
     public GL gl;
     Material material;
+    float x;
+    float y;
+    float z;
+    float sx;
+    float sy;
+    float sz;
 
     public Cubo(GL gl) {
         this.gl = gl;
     }
 
+
+
     public Cubo(GL gl, int x, int y, int z) {
         this.gl = gl;
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
 
     public Cubo(GL gl, Material m) {
@@ -30,45 +42,72 @@ public class Cubo {
     }
 
     public void draw() {
+//gl.glTexEnvi(gl.GL_TEXTURE_ENV, gl.GL_TEXTURE_ENV_MODE, gl.GL_DECAL);
+        gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_S, gl.GL_CLAMP);
+        gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_T, gl.GL_CLAMP);
 
         //***********front*******
         gl.glBegin(GL.GL_QUADS);
+
         gl.glNormal3f(0, 0, 1);
+        gl.glTexCoord2f(0, 0);
         gl.glVertex3f(-1f, 1f, 1f);
+
+        gl.glTexCoord2f(1, 0);
         gl.glVertex3f(1f, 1f, 1f);
+
+        gl.glTexCoord2f(1, 1);
         gl.glVertex3f(1f, -1f, 1f);
+
+        gl.glTexCoord2f(0, 1);
         gl.glVertex3f(-1f, -1f, 1f);
         gl.glEnd();
 
         //***********Back*******
         gl.glBegin(GL.GL_QUADS);
- gl.glNormal3f(0, 0, -1);
+        gl.glNormal3f(0, 0, -1);
+              gl.glTexCoord2f(0, 0);
         gl.glVertex3f(-1f, 1f, -1f);
+              gl.glTexCoord2f(1, 0);
         gl.glVertex3f(1f, 1f, -1f);
+              gl.glTexCoord2f(1, 1);
         gl.glVertex3f(1f, -1f, -1f);
+        
+              gl.glTexCoord2f(0, 1);
         gl.glVertex3f(-1f, -1f, -1f);
         gl.glEnd();
 
         //***********LEft*******
         gl.glBegin(GL.GL_QUADS);
- gl.glNormal3f(-1, 0, 0);
+        gl.glNormal3f(-1, 0, 0);
+              gl.glTexCoord2f(0, 0);
         gl.glVertex3f(-1f, 1f, -1f);
+              gl.glTexCoord2f(1, 0);
         gl.glVertex3f(-1f, 1f, 1f);
+              gl.glTexCoord2f(1, 1);
         gl.glVertex3f(-1f, -1f, 1f);
+              gl.glTexCoord2f(0, 1);
         gl.glVertex3f(-1f, -1f, -1f);
         gl.glEnd();
 
         //***********Right*******
         gl.glBegin(GL.GL_QUADS);
- gl.glNormal3f(1, 0, 0);
+        gl.glNormal3f(1, 0, 0);
+              gl.glTexCoord2f(0, 0);
         gl.glVertex3f(1f, 1f, -1f);
+        
+              gl.glTexCoord2f(1, 0);
         gl.glVertex3f(1f, 1f, 1f);
+        
+              gl.glTexCoord2f(1, 1);
         gl.glVertex3f(1f, -1f, 1f);
+        
+              gl.glTexCoord2f(0, 1);
         gl.glVertex3f(1f, -1f, -1f);
         gl.glEnd();
         //***********top*******
         gl.glBegin(GL.GL_QUADS);
- gl.glNormal3f(0, 1, 0);
+        gl.glNormal3f(0, 1, 0);
         gl.glVertex3f(-1f, 1f, -1f);
         gl.glVertex3f(1f, 1f, -1f);
         gl.glVertex3f(1f, 1f, 1f);
@@ -77,13 +116,29 @@ public class Cubo {
 
         //***********botton*******
         gl.glBegin(GL.GL_QUADS);
-         gl.glNormal3f(0, -1, 0);
+        gl.glNormal3f(0, -1, 0);
         gl.glVertex3f(-1f, -1f, -1f);
         gl.glVertex3f(1f, -1f, -1f);
         gl.glVertex3f(1f, -1f, 1f);
         gl.glVertex3f(-1f, -1f, 1f);
         gl.glEnd();
 
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public float getWidth() {
+        return 3;
     }
 
 }
